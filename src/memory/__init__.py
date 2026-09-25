a572f475540399012346aca5e573c1d1e727b676
Raghul Sambasivam <raghulsam2009@gmail.com>
2026-09-25 20:48:19 +0530

    Stop tracking Python cache files

--- src/memory/__init__.py
+++ src/memory/__init__.py
@@ -3,6 +3,6 @@ Permanent Memory package for ARIA.
 Provides local persistent storage using SQLite.
 """
 
-from .store import MemoryStore
+from .store import MemoryStore, canonicalize_memory_key
 
-__all__ = ["MemoryStore"]
+__all__ = ["MemoryStore", "canonicalize_memory_key"]
